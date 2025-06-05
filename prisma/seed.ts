33b48ef06f817f52865e6f6cfd958776159b849c
aryndraa <aryacoder1102@gmail.com>
2025-06-05 19:58:04 +0800

    disabel database seeder

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,13 +1,24 @@
 import { PrismaClient } from "@prisma/client";
-import { createUserData } from "./factories/user.factory";
-import { createGroupData } from "./factories/group.factory";
+import { faker } from "@faker-js/faker";
 
 const prisma = new PrismaClient();
 
 async function main() {
   for (let i = 0; i < 5; i++) {
-    await prisma.user.create({ data: createUserData() });
-    await prisma.user.create({ data: createGroupData() });
+    await prisma.user.create({
+      data: {
+        displayName: faker.person.fullName(),
+        avatarUrl: faker.image.avatar(),
+      },
+    });
+
+    await prisma.group.create({
+      data: {
+        name: faker.commerce.department(),
+        description: faker.word.words(20),
+        deadlineProject: faker.date.future(),
+      },
+    });
   }
 }
 
