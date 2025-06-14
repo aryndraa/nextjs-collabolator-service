f2ca6c97083ee2cd387f603e510a909091654fa4
aryndraa <aryacoder1102@gmail.com>
2025-06-15 01:11:40 +0800

    sidebar

--- app/_components/app-sidebar.tsx
+++ app/_components/app-sidebar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Sidebar,
   SidebarContent,
@@ -10,18 +12,41 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
-import { LayoutDashboard } from "lucide-react";
+import { RiDashboardFill } from "react-icons/ri";
 import Logo from "./logo";
+import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
 
 const items = [
   {
     title: "Dashboard",
     url: "#",
-    icon: LayoutDashboard,
+    icon: RiDashboardFill,
   },
 ];
 
+type Group = {
+  id: number;
+  name: string;
+};
+
 export function AppSidebar() {
+  const [groups, setGroups] = useState<Group[]>([]);
+
+  useEffect(() => {
+    async function fetchGroups() {
+      try {
+        const res = await fetch("/api/group");
+        const data = await res.json();
+        setGroups(data.data || []); // tergantung struktur GetGroupsResource
+      } catch (err) {
+        console.error("Gagal mengambil grup", err);
+      }
+    }
+
+    fetchGroups();
+  }, []);
+
   return (
     <Sidebar>
       <SidebarHeader className="px-5 py-6">
@@ -29,19 +54,48 @@ export function AppSidebar() {
       </SidebarHeader>
       <SidebarContent className="px-3 ">
         <SidebarGroup>
-          <SidebarGroupLabel>Pages</SidebarGroupLabel>
+          <SidebarGroupLabel className="text-sm text-zinc-400 mb-1">
+            Pages
+          </SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
               {items.map((item) => (
                 <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
+                  <SidebarMenuButton asChild isActive className="py-6 px-4">
                     <a href={item.url}>
-                      <item.icon />
+                      <item.icon className="text-2xl" />
                       <span>{item.title}</span>
                     </a>
                   </SidebarMenuButton>
                 </SidebarMenuItem>
               ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        <SidebarGroup>
+          <SidebarGroupLabel className="text-sm text-zinc-400 mb-1">
+            Group
+          </SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {groups.length === 0 ? (
+                <div className="w-full flex justify-center  text-zinc-400">
+                  <p>No Group</p>
+                </div>
+              ) : (
+                groups.map((group) => (
+                  <SidebarMenuItem key={group.id}>
+                    <SidebarMenuButton asChild className="py-6 px-4">
+                      <a href={`/group/${group.id}`}>
+                        <span>{group.name}</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))
+              )}
+              <Button className="bg-primary-100 hover:bg-primary-300 py-6 text-white mt-4">
+                Create Group
+              </Button>
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
