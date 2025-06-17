3d9ebe10de905c9e39d06c42572cbf7cbe0a6515
aryndraa <aryacoder1102@gmail.com>
2025-06-18 01:46:22 +0800

    group item

--- app/_components/group-item.tsx
+++ app/_components/group-item.tsx
@@ -1,5 +1,20 @@
+import Link from "next/link";
 import React from "react";
+import { FaUsers } from "react-icons/fa";
 
 export default function GroupItem() {
-  return <div>group-item</div>;
+  return (
+    <Link
+      href="#"
+      className="px-6 py-4 hover:bg-zinc-50 flex items-center justify-between transition ease-in-out "
+    >
+      <div className="flex items-center gap-4  text-zinc-400">
+        <FaUsers className="text-2xl" />
+        <span className="font-">Group Name</span>
+      </div>
+      <div className="size-6  text-xs bg-primary-100 font-medium text-white flex items-center justify-center rounded-full ">
+        12
+      </div>
+    </Link>
+  );
 }
--- app/_components/group-links.tsx
+++ app/_components/group-links.tsx
@@ -1,28 +1,26 @@
-import { Input } from "@/components/ui/input";
 import React from "react";
 import Logo from "./logo";
 import { GoPlus } from "react-icons/go";
+import GroupItem from "./group-item";
 
 export default function GroupLinks() {
   return (
-    <div className="w-64 border-r h-screen">
-      <div className="w-full flex justify-start p-5 border-b ">
+    <div className="w-68 border-r h-screen">
+      <div className="w-full flex justify-start p-6 border-b ">
         <Logo />
       </div>
-      <div className="">
-        <div className="flex justify-between pb-4 mb-4 border-b py-4 px-5">
+      <div className="mb-6">
+        <div className="flex justify-between pb-4 mb-4 border-b py-4 px-6">
           <h2>All Group</h2>
           <button className="text-2xl cursor-pointer">
             <GoPlus />
           </button>
         </div>
-        <div className=" px-5">
-          <Input
-            type="text"
-            placeholder="Search Group"
-            className="py-5 shadow-none focus:border-zinc-200! focus:ring-0! rounded-md "
-          />
-        </div>
+      </div>
+      <div className="flex flex-col ">
+        <GroupItem />
+        <GroupItem />
+        <GroupItem />
       </div>
     </div>
   );
