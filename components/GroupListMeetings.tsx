53e5e2727f926deba2949472bbd168d65373d681
aryndraa <aryacoder1102@gmail.com>
2025-06-24 17:30:43 +0800

    new meeting btn

--- components/GroupListMeetings.tsx
+++ components/GroupListMeetings.tsx
@@ -1,23 +1,34 @@
 import React from "react";
 import GroupMeetingCard from "./GroupMeetingCard";
+import { GoPlus } from "react-icons/go";
 
 export default function GroupListMeetings() {
   return (
-    <div className="flex flex-col gap-4 overflow-y-scroll max-h-[80dvh] scroll-y">
-      <div>
-        <h2 className="mb-3 text-sm font-medium text-zinc-400">On Going</h2>
-        <div className="flex flex-col gap-4 ">
-          <GroupMeetingCard />
+    <div>
+      <div className="flex flex-col gap-4 overflow-y-scroll max-h-[73dvh] scroll-y">
+        <div>
+          <h2 className="mb-3 text-sm font-medium text-zinc-400">On Going</h2>
+          <div className="flex flex-col gap-4 ">
+            <GroupMeetingCard />
+          </div>
+        </div>
+        <div>
+          <h2 className="mb-3 text-sm font-medium text-zinc-400">
+            Upcoming Meeting
+          </h2>
+          <div className="flex flex-col gap-4 ">
+            <GroupMeetingCard />
+            <GroupMeetingCard />
+          </div>
         </div>
       </div>
-      <div>
-        <h2 className="mb-3 text-sm font-medium text-zinc-400">
-          Upcoming Meeting
-        </h2>
-        <div className="flex flex-col gap-4 ">
-          <GroupMeetingCard />
-          <GroupMeetingCard />
-        </div>
+      <div className="py-3 w-full border-t ">
+        <button className="flex items-center gap-2 cursor-pointer justify-center p-3 font-medium text-white w-full text-sm bg-primary-100 rounded-lg">
+          <span className="text-xl">
+            <GoPlus />
+          </span>
+          New Meeting
+        </button>
       </div>
     </div>
   );
