3d874a40ac06c680850ad179ac280f1e0f60a0af
aryndraa <aryacoder1102@gmail.com>
2025-06-24 17:16:38 +0800

    tab bar

--- components/GroupBar.tsx
+++ components/GroupBar.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import GroupListMeetings from "./GroupListMeetings";
 import GroupListAssignment from "./GroupListAssignment";
+import GroupBarTab from "./GroupBarTab";
 
 export default function GroupBar() {
   const [tabCon, setTabCon] = useState<boolean>(false);
@@ -10,20 +11,20 @@ export default function GroupBar() {
     <div className="lg:h-[95dvh] bg-white w-[35%] rounded-lg p-6">
       <div className="flex gap-4 mb-6">
         <button
-          className={`px-6 py-2 rounded-full transition cursor-pointer ${
+          className={`pr-6 pl-2 py-2 rounded-full transition cursor-pointer ${
             !tabCon ? "bg-primary-100 text-white" : "bg-white text-zinc-500"
           }`}
           onClick={() => setTabCon(false)}
         >
-          Meetings
+          <GroupBarTab count={0} name="Meetings" active={!tabCon} />
         </button>
         <button
-          className={`px-6 py-2 rounded-full transition cursor-pointer ${
-            tabCon ? "bg-primary-100 text-white" : "bg-white text-zinc-500"
+          className={`pr-6 pl-2 py-2 rounded-full transition cursor-pointer ${
+            tabCon ? "bg-primary-100 text-white " : "bg-white text-zinc-500"
           }`}
           onClick={() => setTabCon(true)}
         >
-          Assigments
+          <GroupBarTab count={12} name="Assignments" active={tabCon} />
         </button>
       </div>
 
