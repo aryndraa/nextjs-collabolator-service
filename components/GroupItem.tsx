290275b7e8154d7e909eb16eca010f1945923c09
aryndraa <aryacoder1102@gmail.com>
2025-07-01 02:10:08 +0800

    group item skeleton

--- components/GroupItem.tsx
+++ components/GroupItem.tsx
@@ -1,41 +1,14 @@
 import { useGroup } from "@/contexts/GroupContext";
 import React from "react";
 import { FaUsers } from "react-icons/fa";
-import { Skeleton } from "./ui/skeleton";
 
-export default function GroupItem({ group, loading = false }: any) {
+export default function GroupItem({ group }: any) {
   const { setGroupId } = useGroup();
   console.log();
 
-  if (loading) {
-    <div>
-      <div className=" px-6 py-4 flex gap-2">
-        <Skeleton className="size-12  rounded-full" />
-        <div>
-          <Skeleton className="h-8  w-40 mb-2  rounded-lg" />
-          <Skeleton className="h-4  w-40 rounded-lg" />
-        </div>
-      </div>
-      <div className=" px-6 py-4 flex gap-2">
-        <Skeleton className="size-12  rounded-full" />
-        <div>
-          <Skeleton className="h-8  w-40 mb-2  rounded-lg" />
-          <Skeleton className="h-4  w-40 rounded-lg" />
-        </div>
-      </div>
-      <div className=" px-6 py-4 flex gap-2">
-        <Skeleton className="size-12  rounded-full" />
-        <div>
-          <Skeleton className="h-8  w-40 mb-2  rounded-lg" />
-          <Skeleton className="h-4  w-40 rounded-lg" />
-        </div>
-      </div>
-    </div>;
-  }
-
   return (
     <button
-      className="px-6 py-4 mx-3 lg:mx-0 rounded-lg lg:rounded-none cursor-pointer hover:bg-zinc-50 flex items-center justify-between transition ease-in-out  bg-white "
+      className="px-6 py-4 mx-3 lg:mx-0 rounded-lg lg:rounded-none cursor-pointer hover:bg-zinc-50 flex items-center justify-between transition ease-in-out  bg-white w-full "
       onClick={() => setGroupId(group.id)}
     >
       <div className="flex items-center gap-4  text-zinc-600">
