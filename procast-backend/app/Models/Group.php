b5518525f47a92c0a088b0b088cc77dcc21b0dae
aryndraa <aryacoder1102@gmail.com>
2025-07-03 21:48:31 +0800

    group & participant model

---
+++ procast-backend/app/Models/Group.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Group extends Model
+{
+    protected $fillable = [
+        'name',
+        'description',
+        'deadline_project',
+    ];
+
+    public function participants(): HasMany
+    {
+        return $this->hasMany(GroupParticipant::class);
+    }
+}
