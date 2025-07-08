b7a1afd52d05660308541d65e0436d41d39be3ad
aryndraa <aryacoder1102@gmail.com>
2025-07-09 01:46:08 +0800

    index resource

--- procast-backend/app/Http/Controllers/Api/V1/Meeting/MeetingController.php
+++ procast-backend/app/Http/Controllers/Api/V1/Meeting/MeetingController.php
@@ -4,18 +4,22 @@ namespace App\Http\Controllers\Api\V1\Meeting;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\V1\Meeting\UpSerMeetingRequest;
+use App\Http\Resources\Api\V1\Meeting\IndexResource;
 use App\Models\Group;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class MeetingController extends Controller
 {
-    public function index(Group $group)
+    public function index(Group $group): AnonymousResourceCollection
     {
         $meetings = $group
             ->meetings()
+            ->newQuery()
+            ->orderBy('date', 'desc')
             ->get();
 
-        return response()->json($meetings);
+        return IndexResource::collection($meetings);
     }
 
     public function store(UpSerMeetingRequest $request, Group $group)
