2aa7e0a533a68d29f82485d098c89d851cd85a7d
aryndraa <aryacoder1102@gmail.com>
2025-07-12 00:05:52 +0800

    form login

--- procast-frontend/components/auth/LoginForm.tsx
+++ procast-frontend/components/auth/LoginForm.tsx
@@ -1,56 +1,38 @@
-import { Button } from "@/components/ui/button";
 import {
   Card,
-  CardAction,
   CardContent,
   CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import Button from "../Button";
+import { InputLabel } from "../InputLabel";
 
 export function LoginForm() {
   return (
     <Card className="w-full max-w-sm">
       <CardHeader>
-        <CardTitle>Login to your account</CardTitle>
+        <CardTitle className="text-2xl">Login to your account</CardTitle>
         <CardDescription>
           Enter your email below to login to your account
         </CardDescription>
-        <CardAction>
-          <Button variant="link">Sign Up</Button>
-        </CardAction>
       </CardHeader>
       <CardContent>
         <form>
           <div className="flex flex-col gap-6">
-            <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                type="email"
-                placeholder="m@example.com"
-                required
-              />
-            </div>
-            <div className="grid gap-2">
-              <div className="flex items-center">
-                <Label htmlFor="password">Password</Label>
-              </div>
-              <Input id="password" type="password" required />
-            </div>
+            <InputLabel
+              name="email"
+              placeholder="example@gmail.com"
+              type="text"
+            />
+            <InputLabel name="password" placeholder="******" type="password" />
           </div>
         </form>
       </CardContent>
       <CardFooter className="flex-col gap-2">
-        <Button type="submit" className="w-full">
-          Login
-        </Button>
-        <Button variant="outline" className="w-full">
-          Login with Google
-        </Button>
+        <Button>Login</Button>
+        <Button type="secondary">Sign Up</Button>
       </CardFooter>
     </Card>
   );
