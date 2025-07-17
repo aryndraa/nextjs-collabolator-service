aa9477128124932673c74b9eb088c2fc1a3b826c
aryndraa <aryacoder1102@gmail.com>
2025-07-17 19:23:09 +0800

    api service to create group

--- procast-frontend/utils/services/group.ts
+++ procast-frontend/utils/services/group.ts
@@ -1,7 +1,14 @@
+import { Group } from "@/lib/stores/group";
 import axios from "@/utils/axios";
 
 export async function getGroups() {
   const response = await axios.get("/api/group/");
+
+  return response.data;
+}
+
+export async function createGroup(group: Omit<Group, "id">): Promise<Group> {
+  const response = await axios.post("/api/group/", JSON.stringify(group));
 
   return response.data;
 }
