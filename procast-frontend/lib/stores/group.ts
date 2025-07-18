213126ae2bcd33a9be44104d5100cd4e1341f485
aryndraa <aryacoder1102@gmail.com>
2025-07-18 20:53:20 +0800

    state to show group

--- procast-frontend/lib/stores/group.ts
+++ procast-frontend/lib/stores/group.ts
@@ -17,6 +17,7 @@ interface GroupState {
   groups: Groups | null;
   group: Group | null;
   setGroups: (groups: Groups) => void;
+  groupCache: Record<string, Group>;
   showGroup: (group: Group) => void;
   optimisticCreateGroup: (group: Group) => void;
 }
@@ -25,7 +26,16 @@ export const useGroup = create<GroupState>()((set) => ({
   groups: null,
   group: null,
   setGroups: (groups) => set({ groups }),
-  showGroup: (group) => set({ group }),
+  groupCache: {},
+  showGroup: (group) => {
+    set((state) => ({
+      group,
+      groupCache: {
+        ...state.groupCache,
+        [group.id]: group,
+      },
+    }));
+  },
   optimisticCreateGroup: (newGroup) =>
     set((state) => ({ groups: [...(state.groups ?? []), newGroup] })),
 }));
